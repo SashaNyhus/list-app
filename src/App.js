6aceb6a4d246b04190c9141ad7b50923815c90f9
SashaVM <sashavictoriam@icloud.com>
2021-05-22 15:52:24 -0700

    added image and css, deleted unused files

--- src/App.js
+++ src/App.js
@@ -2,32 +2,41 @@ import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import {startingList} from './modules/starting-list.js';
 import './App.css';
-import { TaskButton } from './modules/task-button.js';
+
 
 function App() {
   const [taskArray, setList] = useState(startingList);
 
-  function TaskButton(props){
+  const checkTask = (indexOfTask) => {
+    let newTaskArray = [...taskArray];
+    newTaskArray[indexOfTask]['checked'] = !(taskArray[indexOfTask]['checked']);
+    setList(newTaskArray);
+  }
+
+  const TaskList = (props) => {
+    let buttonArray = props.taskArray.map((taskObj, taskIndex) => {
+      return <TaskButton taskInfo={taskObj} taskIndex={taskIndex}/>
+    })
+    return <div className="list-box">
+      {buttonArray}
+    </div>
+  }
+
+  const TaskButton = (props) => {
     return(
         <h2 
             className={props.taskInfo.checked ? "completed-task": "todo-task"}
-            onClick={() => {
-                console.log("test")
-                let newTaskArray = [...taskArray]
-                newTaskArray[props.taskIndex]['checked'] = !(taskArray[props.taskIndex]['checked']);
-                setList(newTaskArray);
-            }}
+            onClick={() => checkTask(props.taskIndex)}
             >
             {props.taskInfo.taskName}
         </h2>
     )
 }
 
-  return (
-    <div>
-      <TaskButton taskInfo={taskArray[2]} taskIndex={2} setList={setList}/>
-    </div>
-  );
+  return <div className="main-box">
+    <TaskList taskArray={taskArray} />
+      {/* <TaskButton taskInfo={taskArray[2]} taskIndex={2} setList={setList}/> */}
+    </div>;
 }
 
 
